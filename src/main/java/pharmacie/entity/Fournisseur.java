c86a5083bb7c8bbcd382b26aef43c7460e9eacb1
Motis <vpnloicvpn@gmail.com>
2026-02-11 10:50:11 +0100

    upd

--- src/main/java/pharmacie/entity/Fournisseur.java
+++ src/main/java/pharmacie/entity/Fournisseur.java
@@ -7,11 +7,34 @@ import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Basic;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+
+
+@Entity
+@Getter @Setter @ToString
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = {"NOM_REFERENCE", "ADRESSEELECTRONIQUE_REFERENCE"})
+})
 public class Fournisseur {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(nullable = false)
+	@Setter(AccessLevel.NONE) // la clé est auto-générée par la BD, On ne veut pas de "setter"
+	private Integer id;
+
     @Basic(optional = false)
     @NonNull
     @Size(min = 1, max = 40)
